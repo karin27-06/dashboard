aa8ed5129a4facd80125ca9f6e6fe741242cff2d
karin27-06 <karinsntmr@gmail.com>
2025-02-25 10:02:18 -0500

    Cambios en user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -35,11 +35,17 @@ class UserSeeder extends Seeder
             'email' => 'gustavo@gmail.com',
             'password' => Hash::make('gustavo123'),
         ]);
+        $user3 = User::create([
+            'name' => 'Karin Santamaria',
+            'email' => 'karinsntmr@gmail.com',
+            'password' => Hash::make('karin2706'),
+        ]);
         // link the roles with the permissions
         $role_admin->syncPermissions($permissions_admin);
         $role_sales->syncPermissions($permissions_sales);
         // assuming the roles of the users
         $user->assignRole($role_admin);
-        $user2->assignRole($role_sales);
+        $user2->assignRole($role_admin);
+        $user3->assignRole($role_admin);
     }
 }
